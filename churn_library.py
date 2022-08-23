fe0f382d1a0f08a6f1252939d5f99ff2fb5ad520
unknown <geo4diale@gmail.com>
2022-08-23 13:22:39 +0300

    Implemented code until encoding of categorical features

--- churn_library.py
+++ churn_library.py
@@ -1,54 +1,143 @@
-# library doc string
+"""
+This module is a library of functions to find customers who are likely to churn.
 
+Author: George Dialektakis
+Date: August 2022
+"""
 
 # import libraries
 import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
 
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+
+import shap
+import joblib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
 
 
 def import_data(pth):
-    '''
+    """
     returns dataframe for the csv found at pth
 
     input:
             pth: a path to the csv
     output:
-            df: pandas dataframe
-    '''	
-	pass
+            data_df: pandas dataframe
+    """
+    data_df = pd.read_csv(pth)
+    print(data_df.head())
+    return data_df
 
 
-def perform_eda(df):
-    '''
-    perform eda on df and save figures to images folder
+def perform_eda(data_df):
+    """
+    perform eda on data_df and save figures to images folder
     input:
-            df: pandas dataframe
+            data_df: pandas dataframe
 
     output:
             None
-    '''
-	pass
+    """
+    print(data_df.shape)
+    print(data_df.isnull().sum())
+    print(data_df.describe())
+
+    cat_columns = [
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category'
+    ]
+
+    quant_columns = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio'
+    ]
+
+    data_df['Churn'] = data_df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+
+    plt.figure(figsize=(20, 10))
+    data_df['Churn'].hist()
+    plt.savefig('./images/eda/churn_histogram.png')
+
+    plt.figure(figsize=(20, 10))
+    data_df['Customer_Age'].hist()
+    plt.savefig('./images/eda/customer_age_histogram.png')
+
+    plt.figure(figsize=(20, 10))
+    data_df.Marital_Status.value_counts('normalize').plot(kind='bar')
+    plt.savefig('./images/eda/marital_status_histogram.png')
+
+    plt.figure(figsize=(20, 10))
+    # distplot is deprecated. Use histplot instead
+    # sns.distplot(df['Total_Trans_Ct']);
+    # Show distributions of 'Total_Trans_Ct' and add a smooth curve obtained using a kernel density estimate
+    sns.histplot(data_df['Total_Trans_Ct'], stat='density', kde=True)
+    plt.savefig('./images/eda/total_trans_histogram.png')
+
+    plt.figure(figsize=(20, 10))
+    sns.heatmap(data_df.corr(), annot=False, cmap='Dark2_r', linewidths=2)
+    plt.savefig('./images/eda/correlation_heatmap.png')
+    # plt.show()
 
 
-def encoder_helper(df, category_lst, response):
-    '''
+def encoder_helper(data_df, category_lst, response=None):
+    """
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
 
     input:
-            df: pandas dataframe
+            data_df: pandas dataframe
             category_lst: list of columns that contain categorical features
             response: string of response name [optional argument that could be used for naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for
-    '''
-    pass
+    """
+
+    # encode columns that belong to category list
+    for feature in category_lst:
+        feature_lst = []
+        feature_groups = data_df.groupby(feature).mean()['Churn']
+
+        for val in data_df[feature]:
+            feature_lst.append(feature_groups.loc[val])
+
+        column_name = str(feature) + '_Churn'
+        data_df[column_name] = feature_lst
+
+    return data_df
 
 
-def perform_feature_engineering(df, response):
-    '''
+def perform_feature_engineering(data_df, response):
+    """
     input:
               df: pandas dataframe
               response: string of response name [optional argument that could be used for naming variables or index y column]
@@ -58,7 +147,11 @@ def perform_feature_engineering(df, response):
               X_test: X testing data
               y_train: y training data
               y_test: y testing data
-    '''
+    """
+
+    y = data_df['Churn']
+    X = pd.DataFrame()
+
 
 def classification_report_image(y_train,
                                 y_test,
@@ -66,7 +159,7 @@ def classification_report_image(y_train,
                                 y_train_preds_rf,
                                 y_test_preds_lr,
                                 y_test_preds_rf):
-    '''
+    """
     produces classification report for training and testing results and stores report as image
     in images folder
     input:
@@ -79,12 +172,12 @@ def classification_report_image(y_train,
 
     output:
              None
-    '''
+    """
     pass
 
 
 def feature_importance_plot(model, X_data, output_pth):
-    '''
+    """
     creates and stores the feature importances in pth
     input:
             model: model object containing feature_importances_
@@ -93,8 +186,9 @@ def feature_importance_plot(model, X_data, output_pth):
 
     output:
              None
-    '''
+    """
     pass
+
 
 def train_models(X_train, X_test, y_train, y_test):
     '''
@@ -108,3 +202,10 @@ def train_models(X_train, X_test, y_train, y_test):
               None
     '''
     pass
+
+
+if __name__ == "__main__":
+    data_df = import_data("./data/bank_data.csv")
+    perform_eda(data_df)
+    category_lst = ['Gender', 'Education_Level', 'Marital_Status', 'Income_Category', 'Card_Category', 'Card_Category']
+    encoder_helper(data_df, category_lst)
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,63 +1,84 @@
+"""
+A module to create logging and tests
+
+Author: George Dialektakis
+Date: August 2022
+"""
+
 import os
 import logging
-import churn_library_solution as cls
+from churn_library import import_data, perform_eda
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+
+# import churn_library_solution as cls
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+def test_import(import_data):
+    """
+        test data import - this example is completed for you to assist with the other test functions
+    """
+    try:
+        data_df = import_data("./data/bank_data.csv")
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    try:
+        assert data_df.shape[0] > 0
+        assert data_df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_eda(perform_eda):
-	'''
-	test perform eda function
-	'''
+    """
+        test perform eda function
+    """
+    try:
+        perform_eda(import_data("./data/bank_data.csv"))
 
+        # path of the directory of eda images
+        path = "./images/eda"
+        # Getting the list of directories
+        dir = os.listdir(path)
+
+        # Checking if the list is empty or not
+        assert len(dir) != 0
+        logging.info("Testing perform_eda: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Testing perform_eda: The folder images/eda is empty which means eda images haven't been saved properly.")
+        raise err
 
 def test_encoder_helper(encoder_helper):
-	'''
-	test encoder helper
-	'''
+    """
+        test encoder helper
+        """
 
 
 def test_perform_feature_engineering(perform_feature_engineering):
-	'''
-	test perform_feature_engineering
-	'''
+    """
+        test perform_feature_engineering
+        """
 
 
 def test_train_models(train_models):
-	'''
-	test train_models
-	'''
+    """
+        test train_models
+        """
 
 
 if __name__ == "__main__":
-	pass
-
-
-
-
-
-
-
+    test_import(import_data)
+    test_eda(perform_eda)
 
